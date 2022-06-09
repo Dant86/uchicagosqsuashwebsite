8223009d165a5b632782499607b18480c3b7a4af
Vedant Pathak <vedantdpathak@iCloud.com>
2022-06-08 20:39:36 -0400

    Roster styling

--- uchisquashsite/routes/base.py
+++ uchisquashsite/routes/base.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template
+from requests import get
 from uchisquashsite.queries.player import get_active_roster
 
 base = Blueprint('base', __name__)
+
+CHRIS_URL = 'https://api.ussquash.com/resources/res/user/204905/rankings'
 
 @base.route('/')
 def homepage():
@@ -13,5 +16,8 @@ def about():
 
 @base.route('/roster')
 def roster():
-    roster = [(p, f'{r:.2f}', x) for p, r, x in sorted(get_active_roster(), key=lambda x: x[2])]
-    return render_template('roster.html', roster=roster)
+    roster_sorted = sorted(get_active_roster(), key=lambda x: x[2])
+    roster = [(p, f'{r:.2f}', x) for p, r, x in roster_sorted]
+    l = len(roster)
+    chris_rating = get(CHRIS_URL).json()[0]['Rating']
+    return render_template('roster.html', roster=roster, l=l, chris_rating=chris_rating)
