7b0089fb03a1db3a0723c5fd9a056b0e74d9fa70
Vedant Pathak <vedantdpathak@iCloud.com>
2022-06-09 15:05:07 -0400

    schedule page live

--- uchisquashsite/queries/match.py
+++ uchisquashsite/queries/match.py
@@ -1,14 +1,32 @@
 from uchisquashsite.models import Match
 from uchisquashsite.models import db
+from pandas import DataFrame
+from requests import get
 
-def new_match(home, opp, date):
-    match = Match(home=home, opponent=opp, date=date)
-    db.session.add(match)
-    db.session.commit()
+USSQUASH_SCHEDULE_URL = 'https://api.ussquash.com/resources/teams/29927/schedule'
 
-def log_match_scores(mid, our_s, their_s):
-    match = Match.query.filter_by(id=mid)
-    match.our_score = our_s
-    match.their_score = their_s
-    match.happened = True
-    db.session.commit()
+MATCH_COLS = ['matchdate', 'hteamid', 'vteamid', 'Home_Matches_Won', 'Visitor_Matches_Won', 'wTeamName', 'oTeamName']
+
+def opposing_team(row):
+    return row['oTeamName'] if row['hteamid'] == 29927 else row['wTeamName']
+
+def opposing_score(row):
+    return row['Visitor_Matches_Won'] if row['hteamid'] == 29927 else row['Home_Matches_Won']
+
+def home(row):
+    return row['hteamid'] == 29927
+
+def process_df(df):
+    oteam = df.apply(opposing_team, axis=1)
+    oscore = df.apply(opposing_score, axis=1)
+    df['home'] = df.apply(home, axis=1)
+    newdf = DataFrame({'date': df['matchdate'], 'opp': oteam, 'oscore': oscore, 'home': df['home']})
+    newdf['ourscore'] = 9 - newdf['oscore']
+    print(newdf)
+    return newdf
+
+def get_all_matches():
+    r = get(USSQUASH_SCHEDULE_URL)
+    matches = r.json()
+    matches_df = DataFrame(matches)
+    return process_df(matches_df[MATCH_COLS])
--- uchisquashsite/routes/base.py
+++ uchisquashsite/routes/base.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from requests import get
+from pandas import DataFrame, to_datetime
 from uchisquashsite.queries.player import get_active_roster
+from uchisquashsite.queries.match import get_all_matches
 
 base = Blueprint('base', __name__)
 
 CHRIS_URL = 'https://api.ussquash.com/resources/res/user/204905/rankings'
+STANDINGS_URL = 'https://api.ussquash.com/resources/divisions/standings/3482'
 
 @base.route('/')
 def homepage():
@@ -12,7 +15,10 @@ def homepage():
 
 @base.route('/about')
 def about():
-    return render_template('about.html')
+    r = get(STANDINGS_URL)
+    df = DataFrame(r.json())
+    rank = int(df[df['teamid'] == 29927]['hGroup'])
+    return render_template('about.html', rank=rank)
 
 @base.route('/roster')
 def roster():
@@ -20,4 +26,13 @@ def roster():
     roster = [(p, f'{r:.2f}', x) for p, r, x in roster_sorted]
     l = len(roster)
     chris_rating = get(CHRIS_URL).json()[0]['Rating']
-    return render_template('roster.html', roster=roster, l=l, chris_rating=chris_rating)
+    return render_template('roster.html', roster=roster, l=l,
+                           chris_rating=chris_rating)
+
+@base.route('/matches')
+def matches():
+    ms = get_all_matches()
+    ms['date'] = to_datetime(ms['date'])
+    ms = ms.sort_values('date')
+    ms['date'] = ms['date'].dt.strftime('%A, %B %d, %Y')
+    return render_template('matches.html', ms=ms)
